c17580fbd9b4c66802a2d704b7a3499c9f699ab7
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2022-10-25 12:43:00 +0400

    Drowing 2 images with differant  possitions

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
-
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
@@ -22,12 +21,21 @@ int	main(void)
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	//void	*mlx;
+	//void	*img;
+	char	*relative_path = "./wall.xpm";
+	char	*relative_path2 = "./floor.xpm";
+	int		img_width;
+	int		img_height;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 55, 55, 0x00FF0000);
+	//img.img = mlx_new_image(mlx, 1920, 1080);
+	img.img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	//img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	//my_mlx_pixel_put(&img, 255, 255, 0x00FF0000);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	img.img = mlx_xpm_file_to_image(mlx, relative_path2, &img_width, &img_height);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 500, 500);
 	mlx_loop(mlx);
 }
